a0343190bc0a04d35695ad9d828b53bc1aa6a6d7
naveendgp <naveendgp@gmail.com>
2023-10-24 14:21:07 +0530

    card for mobile

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -2,48 +2,45 @@ import React, { useState } from "react";
 import PopupCard from "./PopupCard";
 import { Link } from "react-router-dom";
 
-const Card = ({Name,btn,desc,rules,img,link,startDate,endDate}) => {
+const Card = ({ Name, btn, desc, rules, img, link, startDate, endDate }) => {
+  const [popup, setPopup] = useState(false);
 
-    const[popup,setPopup] = useState(false)
-
-    const onClose = ()=>{setPopup(!popup)}
-    return (
-      <div>
-        {popup && <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center z-50">
+  const onClose = () => {
+    setPopup(!popup);
+  };
+  return (
+    <div>
+      {popup && (
+        <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center z-50">
           <div className="bg-white p-4 rounded shadow-lg">
-            <PopupCard close={onClose} startDate={startDate} endDate={endDate} rules={rules} btn={btn}
-              link={link}/>
+            <PopupCard
+              close={onClose}
+              startDate={startDate}
+              endDate={endDate}
+              rules={rules}
+              btn={btn}
+              link={link}
+            />
           </div>
-        </div>}
-        <section className="flex flex-col mb-[100px] ">
-          <div className="flex rounded-[15px]  mt-[50px] ml-[300px] bg-white w-[1000px] h-[400px] transition duration-300 ease-in-out border border-bg-home hover:border-blue-500">
-            
-            <div className="flex ml-[20px] bg-transparent mt-[20px] ">
-              <img
-                src={img}
-                alt=""
-                className="w-[300px] h-[350px] rounded-lg"
-              />
-            </div>
-            <div className="flex flex-col bg-transparent  items-center  ml-[80px] w-[500px]">
-              <h3 className="text-2xl bg-transparent mt-4">{Name}</h3>
-              <span className="border-1 border-blue rounded-full w-full h-1 mt-3 bg-slate-800"></span>
-              <p className="mt-[50px] bg-transparent m-8">{desc}</p>
-              <span className="flex flex-row bg-transparent items-center">
-                <p className="mr-[100px] align-center text-px bg-transparent">
-                  End date: {endDate}
-                </p>
-                <button className="bg-blue-500 text-white rounded-full ml-[50px] py-2 px-4"
-                    onClick={() => setPopup(true)}>
-                  Know More
-                </button>
-              </span>
-              
-            </div>
+        </div>
+      )}
+      <section>
+        <div className="flex flex-col items-center bg-white shadow-lg h-[720px] w-[350px] mx-auto mb-5 rounded-[15px]
+              sm:flex flex-row w-screen ">
+          <div className="border  h-[300px]  rounded-[15px]">
+            <img className="h-[300px] width-[300px] rounded-[15px] mt-[20px]" src={img} alt="" />
           </div>
-        </section>
-      </div>
-    );
-}
+          <div className="flex flex-col py-[10px] mt-[25px] items-center w-[340px] h-[330px]  ">
+            <h3 className="text-2xl">{Name}</h3>
+            <span className="border-2 w-[200px] border-blue-900"></span>
+            <p className="mt-[18px] text-center">{desc}</p>
+            <p className="mt-[20px]">End Date: {endDate}</p>
+          </div>
+          <button onClick={() => setPopup(true)} className="bg-blue-500 mt-3 py-[8px] px-[30px] rounded-full text-white">Know More</button>
+        </div>
+      </section>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
