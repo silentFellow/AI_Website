b931274237d57c20cabc7a7be4a343b28dd5e790
theGreatestIntelligent <thegreatestintelligent@gmail.com>
2023-10-24 19:44:10 +0530

    final changes

--- client/src/components/Usercard.jsx
+++ client/src/components/Usercard.jsx
@@ -15,15 +15,15 @@ const Usercard = ( {userName, role, changed, setChanged} ) => {
   }
 
   return (
-    <div className="h-[8.1rem] w-full md:w-[81%] bg-green-800 rounded-lg flex mt-4">
+    <div className="h-[8.1rem] w-full md:w-[90%] bg-green-800 rounded-lg flex mt-4">
       <section className="h-full w-[69%] p-3 flex  flex-col justify-center items-center">
-        <div className="text-[1.4rem] text-primary opacity-80">{userName}</div>
-        <div className="text-[1.4rem] mt-1 text-primary opacity-80">({role})</div>
+        <div className="text-[1.2rem] text-primary opacity-80">{userName}</div>
+        <div className="text-[1.2rem] mt-1 text-primary opacity-80">({role})</div>
       </section>
       <section className="w-[31%] flex justify-center items-center">
         <RiDeleteBin6Line 
           onClick={deleteUser}
-          className='text-[4.5rem] p-3 cursor-pointer text-primary'
+          className='text-[3.6rem] p-3 cursor-pointer text-primary'
         />
       </section>
     </div>
